28bd321609ea37723a4c6b394f8714248e35e631
Niraj Singh Tomar <32215357+nemosierra@users.noreply.github.com>
2019-03-29 18:19:14 +0530

    Add files via upload

---
+++ src/Component/Titles.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+		<h1 className="title-container__title">Weather Finder</h1>
+		<h3 className="title-container__subtitle">Find out temperature, conditions and more...</h3>
+		<h5 className="title-container__madeby"> Made by Niraj Singh Tomar </h5>
+	</div>
+);
+
+export default Titles;
